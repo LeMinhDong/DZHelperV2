2956833161c6374ccc298745938a99fa58162683
dong <dong@ADMIN-PC>
2025-01-04 00:37:04 +0700

    update LdModel

--- DZHelper/Models/LdModel.cs
+++ DZHelper/Models/LdModel.cs
@@ -4,12 +4,42 @@ namespace DZHelper.Models
 {
     public partial class LdModel:BaseModel
     {
-        [ObservableProperty]
-        private string name;
+        public LdModel()
+        {
+                
+        }
+        public LdModel(string[] args)
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+            }
+        }
+
 
         [ObservableProperty]
         private string index;
 
+        [ObservableProperty]
+        private string name;
+
+        [ObservableProperty]
+        private string title;
+        
+        [ObservableProperty]
+        private bool isOpen;
+
+        [ObservableProperty]
+        private string dataInput;
+
+        public IntPtr TopHandle { get; set; }
+        public IntPtr BindHandle { get; set; }
+        public int AndroidState { get; set; }
+        public int DnplayerPID { get; set; }
+        public int VboxPID { get; set; }
 
     }
 }
